99c3e5ef84d4defd5784bdad699feb57a16ebe16
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2023-01-10 19:59:37 +0800

    denoise modules

--- utils/example.py
+++ utils/example.py
@@ -4,8 +4,8 @@ from utils.vocab import Vocab, LabelVocab
 from utils.word2vec import Word2vecUtils
 from utils.evaluator import Evaluator
 
+class Example():
 
-class Example:
     @classmethod
     def configuration(cls, root, train_path=None, word2vec_path=None):
         cls.evaluator = Evaluator()
@@ -15,33 +15,41 @@ class Example:
 
     @classmethod
     def load_dataset(cls, data_path):
-        dataset = json.load(open(data_path, "r"))
+        datas = json.load(open(data_path, 'r'))
         examples = []
-        for di, data in enumerate(dataset):
-            for ui, utt in enumerate(data):
-                ex = cls(utt, f"{di}-{ui}")
+        for data in datas:
+            for utt in data:
+                ex = cls(utt)
+                # assert False
                 examples.append(ex)
         return examples
 
-    def __init__(self, ex: dict, did):
+    def __init__(self, ex: dict):
         super(Example, self).__init__()
         self.ex = ex
-        self.did = did
+        self.utt = ex['asr_1best']
+        self.denoise_utt = ex['manual_transcript'].replace('(unknown)','').replace('(side)','').replace('(dialect)','').replace('(robot)','').replace('noise','')
 
-        self.utt = ex["asr_1best"]
+        # print(self.utt)
         self.slot = {}
-        for label in ex["semantic"]:
-            act_slot = f"{label[0]}-{label[1]}"
+        for label in ex['semantic']:
+            act_slot = f'{label[0]}-{label[1]}'
+            # print(act_slot)
             if len(label) == 3:
                 self.slot[act_slot] = label[2]
-        self.tags = ["O"] * len(self.utt)
+                # print(label[2])
+        self.tags = ['O'] * len(self.denoise_utt)
+        # print(self.tags)
         for slot in self.slot:
             value = self.slot[slot]
-            bidx = self.utt.find(value)
+            bidx = self.denoise_utt.find(value)
             if bidx != -1:
-                self.tags[bidx : bidx + len(value)] = [f"I-{slot}"] * len(value)
-                self.tags[bidx] = f"B-{slot}"
-        self.slotvalue = [f"{slot}-{value}" for slot, value in self.slot.items()]
+                self.tags[bidx: bidx + len(value)] = [f'I-{slot}'] * len(value)
+                self.tags[bidx] = f'B-{slot}'
+        # print(self.tags)
+        self.slotvalue = [f'{slot}-{value}' for slot, value in self.slot.items()]
         self.input_idx = [Example.word_vocab[c] for c in self.utt]
+        self.denoise_idx = [Example.word_vocab[c] for c in self.denoise_utt]
         l = Example.label_vocab
         self.tag_id = [l.convert_tag_to_idx(tag) for tag in self.tags]
+        # print(self.tag_id)
