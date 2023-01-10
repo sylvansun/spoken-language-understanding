7124adf9cef4bafa67f76b819391018ca75a1eed
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2023-01-10 20:11:18 +0800

    Revert "denoise modules"
    This reverts commit 99c3e5ef84d4defd5784bdad699feb57a16ebe16.

--- utils/batch.py
+++ utils/batch.py
@@ -1,34 +1,25 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 import torch
 
 
-def from_example_list(args, ex_list, device='cpu', train=True):
+def from_example_list(args, ex_list, device="cpu", train=True):
     ex_list = sorted(ex_list, key=lambda x: len(x.input_idx), reverse=True)
     batch = Batch(ex_list, device)
     pad_idx = args.pad_idx
     tag_pad_idx = args.tag_pad_idx
 
     batch.utt = [ex.utt for ex in ex_list]
-    # print(batch.utt)
     input_lens = [len(ex.input_idx) for ex in ex_list]
-    denoise_lens = [len(ex.denoise_idx) for ex in ex_list]
-    for i in range(len(input_lens)):
-        input_lens[i] = max(input_lens[i],denoise_lens[i])
-    # print(input_lens)
     max_len = max(input_lens)
     input_ids = [ex.input_idx + [pad_idx] * (max_len - len(ex.input_idx)) for ex in ex_list]
-    
     batch.input_ids = torch.tensor(input_ids, dtype=torch.long, device=device)
-    # print(batch.input_ids.shape)
     batch.lengths = input_lens
+    batch.did = [ex.did for ex in ex_list]
 
     if train:
-        denoise_ids = [ex.denoise_idx + [pad_idx] * (max_len - len(ex.denoise_idx)) for ex in ex_list]
-        batch.denoise_ids = torch.tensor(denoise_ids, dtype=torch.long, device=device)
-
         batch.labels = [ex.slotvalue for ex in ex_list]
         tag_lens = [len(ex.tag_id) for ex in ex_list]
-        max_tag_lens = max_len
+        max_tag_lens = max(tag_lens)
         tag_ids = [ex.tag_id + [tag_pad_idx] * (max_tag_lens - len(ex.tag_id)) for ex in ex_list]
         tag_mask = [[1] * len(ex.tag_id) + [0] * (max_tag_lens - len(ex.tag_id)) for ex in ex_list]
         batch.tag_ids = torch.tensor(tag_ids, dtype=torch.long, device=device)
@@ -36,12 +27,13 @@ def from_example_list(args, ex_list, device='cpu', train=True):
     else:
         batch.labels = None
         batch.tag_ids = None
-        batch.tag_mask = None
+        tag_mask = [[1] * len(ex.input_idx) + [0] * (max_len - len(ex.input_idx)) for ex in ex_list]
+        batch.tag_mask = torch.tensor(tag_mask, dtype=torch.float, device=device)
+
     return batch
 
 
-class Batch():
-
+class Batch:
     def __init__(self, examples, device):
         super(Batch, self).__init__()
 
--- utils/example.py
+++ utils/example.py
@@ -4,8 +4,8 @@ from utils.vocab import Vocab, LabelVocab
 from utils.word2vec import Word2vecUtils
 from utils.evaluator import Evaluator
 
-class Example():
 
+class Example:
     @classmethod
     def configuration(cls, root, train_path=None, word2vec_path=None):
         cls.evaluator = Evaluator()
@@ -15,41 +15,33 @@ class Example():
 
     @classmethod
     def load_dataset(cls, data_path):
-        datas = json.load(open(data_path, 'r'))
+        dataset = json.load(open(data_path, "r"))
         examples = []
-        for data in datas:
-            for utt in data:
-                ex = cls(utt)
-                # assert False
+        for di, data in enumerate(dataset):
+            for ui, utt in enumerate(data):
+                ex = cls(utt, f"{di}-{ui}")
                 examples.append(ex)
         return examples
 
-    def __init__(self, ex: dict):
+    def __init__(self, ex: dict, did):
         super(Example, self).__init__()
         self.ex = ex
-        self.utt = ex['asr_1best']
-        self.denoise_utt = ex['manual_transcript'].replace('(unknown)','').replace('(side)','').replace('(dialect)','').replace('(robot)','').replace('noise','')
+        self.did = did
 
-        # print(self.utt)
+        self.utt = ex["asr_1best"]
         self.slot = {}
-        for label in ex['semantic']:
-            act_slot = f'{label[0]}-{label[1]}'
-            # print(act_slot)
+        for label in ex["semantic"]:
+            act_slot = f"{label[0]}-{label[1]}"
             if len(label) == 3:
                 self.slot[act_slot] = label[2]
-                # print(label[2])
-        self.tags = ['O'] * len(self.denoise_utt)
-        # print(self.tags)
+        self.tags = ["O"] * len(self.utt)
         for slot in self.slot:
             value = self.slot[slot]
-            bidx = self.denoise_utt.find(value)
+            bidx = self.utt.find(value)
             if bidx != -1:
-                self.tags[bidx: bidx + len(value)] = [f'I-{slot}'] * len(value)
-                self.tags[bidx] = f'B-{slot}'
-        # print(self.tags)
-        self.slotvalue = [f'{slot}-{value}' for slot, value in self.slot.items()]
+                self.tags[bidx : bidx + len(value)] = [f"I-{slot}"] * len(value)
+                self.tags[bidx] = f"B-{slot}"
+        self.slotvalue = [f"{slot}-{value}" for slot, value in self.slot.items()]
         self.input_idx = [Example.word_vocab[c] for c in self.utt]
-        self.denoise_idx = [Example.word_vocab[c] for c in self.denoise_utt]
         l = Example.label_vocab
         self.tag_id = [l.convert_tag_to_idx(tag) for tag in self.tags]
-        # print(self.tag_id)
